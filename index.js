de0c5c2b5842701ea2e8dd6ca4314175ecfdbe77
PrabhavKarve <karve.prabhav@gmail.com>
2025-01-28 23:48:22 -0500

    modified cors, added frontend address, modified vercel.json

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
 app.use(
   cors({
-      origin: "https://f1bnb-frontend.vercel.app/", // Replace with your frontend URL
+      origin: "https://f1bnb-frontend.vercel.app", // Replace with your frontend URL
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true, // If using cookies/sessions
   })
 );
+app.options('*', cors()); 
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Ensure this line is present
 // Routes
