03bc662d8218ecdde140d6d1deba3eee05eb2c3c
PrabhavKarve <karve.prabhav@gmail.com>
2025-01-28 23:28:12 -0500

    added cloudinary added cors

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors());
+app.use(
+  cors({
+      origin: "*", // Replace with your frontend URL
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true, // If using cookies/sessions
+  })
+);
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Ensure this line is present
 // Routes
